476e1c5f242872e6ee5e8be7553da02f248b0b22
Cyril Pierron <cyril.pierron@tutanota.com>
2020-12-10 19:09:42 +0100

    Moving most variables to an external configuration file. Added various checks to validate imported data type and format.

--- cloudsync.py
+++ cloudsync.py
@@ -6,55 +6,8 @@ import os.path
 import subprocess
 import logging
 
-USER_HOME = "/home/gandalf"
-DOCUMENTS = USER_HOME + "/Documents"
-SPIDEROAK_HIVE = USER_HOME + "/SpiderOak Hive"
-INTERNXT_DRIVE = USER_HOME + "/Internxt Drive"
-FILE_LIST = (
-    {'src': DOCUMENTS + "/KeePassXC.kdbx", 'dst': SPIDEROAK_HIVE + "/security"},
-)
-FOLDER_LIST = (
-    {
-        'src': SPIDEROAK_HIVE + "/finance",
-        'dst': INTERNXT_DRIVE,
-        'options': ()
-    },
-    {
-        'src': SPIDEROAK_HIVE + "/documents",
-        'dst': INTERNXT_DRIVE,
-        'options': ()
-    },
-    {
-        'src': SPIDEROAK_HIVE + "/security",
-        'dst': INTERNXT_DRIVE,
-        'options': ("--exclude=security/archives", )
-    },
-    {
-        'src': USER_HOME + "/dev/cloudsync",
-        'dst': SPIDEROAK_HIVE + "/gitbackup",
-        'options': (
-            "--exclude=.git",
-            "--exclude=.idea",
-            "--exclude=venv",
-            "--exclude=*.swp"
-        )
-    },
-    {
-        'src': USER_HOME + "/dev/joplinRevisionsCleanUp",
-        'dst': SPIDEROAK_HIVE + "/gitbackup",
-        'options': (
-            "--exclude=.git",
-            "--exclude=.idea",
-            "--exclude=venv",
-            "--exclude=*.swp"
-        )
-    },
-    {
-        'src': SPIDEROAK_HIVE + "/gitbackup",
-        'dst': INTERNXT_DRIVE,
-        'options': ()
-    },
-)
+from conf import FILE_LIST as FILE_LIST
+from conf import FOLDER_LIST as FOLDER_LIST
 
 
 def filersync(src, dst):
@@ -71,6 +24,12 @@ def syncfile(src, dst):
     """The syncfile(src, dst) function synces a file specified as src with the dst directory (both ways).
     src (string) - the source file's full path (i.e. /path/filename)
     dst (string) - the destination directory's full path to rsync src to"""
+    if not isinstance(src, str) or (isinstance(src, str) and len(src) == 0):
+        logging.error("ERROR (syncfile): improper mandatory src argument! Skipping file sync task.")
+        return False
+    if not isinstance(dst, str) or (isinstance(dst, str) and len(dst) == 0):
+        logging.error("ERROR (syncfile): improper mandatory dst argument! Skipping file sync task.")
+        return False
     if not os.path.isfile(src):
         logging.error("ERROR (syncfile): " + src + " file not found!")
         return False
@@ -100,34 +59,52 @@ def folderrsync(cmd, src, dst):
     if sync.returncode == 0:
         logging.info(src + " directory successfully synced to " + dst + ".")
     else:
-        logging.error("ERROR: something went wrong syncing SpiderOak's " + src + " directory.")
+        logging.error("ERROR: something went wrong syncing the " + src + " directory.")
 
 
-def syncfolder(src, dst, options):
-    """The syncfolder() function keeps directories in sync (one way from src to dst)
-    src (string) - the source directory's full path
-    dst (string) - the destination directory's full path to rsync the src folder to
-    options (list) - additional parameters (like exclusions) to pass to rsync"""
+def syncfolder(config):
+    """The syncfolder() function keeps 2 directories in sync (one way only: from src to dst)
+    config (dict) - rsync configuration information as the following keys:
+        src (string) - the source directory's full path
+        dst (string) - the destination directory's full path to rsync the src folder to
+        options (tuple) - rsync full parameter list"""
+    if not isinstance(config, dict):
+        logging.error("ERROR (syncfolder): wrong argument type! A dictionary is expected.")
+        return False
+    if not all(k in config for k in ('src', 'dst', 'options')):
+        logging.error("ERROR (syncfolder): missing mandatory keys in dictionary argument!")
+        return False
+    src, dst, options = config.values()
+    if not isinstance(src, str) or (isinstance(src, str) and len(src) == 0):
+        logging.error("ERROR (syncfolder): improper mandatory src key argument! Canceling task.")
+        return False
+    if not isinstance(dst, str) or (isinstance(dst, str) and len(dst) == 0):
+        logging.error("ERROR (syncfolder): improper mandatory dst key argument! Canceling task.")
+        return False
+    if not isinstance(options, tuple) or (isinstance(options, tuple) and len(options) == 0):
+        logging.error("ERROR (syncfolder): improper mandatory options key argument! Canceling task.")
+        return False
+    for o in options:
+        if not isinstance(o, str) or (isinstance(o, str) and not o.startswith("-")):
+            logging.error("ERROR (syncfolder): incorrect rsync option! Canceling task.")
+            return False
     if not os.path.isdir(dst):
         logging.error("ERROR (syncfolder): destination directory " + dst + " not found, aborting task!")
     elif not os.path.isdir(src):
         logging.error("ERROR (syncfolder): source directory " + src + " not found, skipping task!")
     else:
-        rsynccmd = "rsync -rpAXogEtu --delete "  # only updates as it's a one way sync
-        if len(options) > 0:
-            rsynccmd += " ".join(options)
-        rsynccmd += " '" + src + "' '" + dst + "'"
+        rsynccmd = "rsync  " + " ".join(options) + " '" + src + "' '" + dst + "'"
         logging.debug(rsynccmd)
         folderrsync(rsynccmd, src, dst)
 
 
 logging.basicConfig(level=logging.INFO)  # systemd works fine with standard outputs (stdout, stderr)
-option = "--all"  # default option
+clarg = "--all"  # default command line argument
 if len(sys.argv) > 1 and sys.argv[1] in ("--spideroak", "--internxt", "--all"):
-    option = sys.argv[1]
-if option in ("--spideroak", "--all"):
+    clarg = sys.argv[1]
+if clarg in ("--spideroak", "--all"):
     for item in FILE_LIST:
         syncfile(item['src'], item['dst'])
-if option in ("--internxt", "--all"):
+if clarg in ("--internxt", "--all"):
     for item in FOLDER_LIST:
-        syncfolder(item['src'], item['dst'], item['options'])
+        syncfolder(item)
---
+++ conf.py
@@ -0,0 +1,72 @@
+# "private" variables for convenience
+
+USER_HOME = "/home/gandalf"
+DOCUMENTS = USER_HOME + "/Documents"
+SPIDEROAK_HIVE = USER_HOME + "/SpiderOak Hive"
+INTERNXT_DRIVE = USER_HOME + "/Internxt Drive"
+
+# only FILE_LIST and FOLDER_LIST are imported in cloudsync.py
+
+FILE_LIST = (
+    {'src': DOCUMENTS + "/KeePassXC.kdbx", 'dst': SPIDEROAK_HIVE + "/security"},
+)
+
+FOLDER_LIST = (
+    {
+        'src': SPIDEROAK_HIVE + "/finance",
+        'dst': INTERNXT_DRIVE,
+        'options': (
+            "-rpAXogEt",
+            "--delete"
+        )
+    },
+    {
+        'src': SPIDEROAK_HIVE + "/documents",
+        'dst': INTERNXT_DRIVE,
+        'options': (
+            "-rpAXogEt",
+            "--delete"
+        )
+    },
+    {
+        'src': SPIDEROAK_HIVE + "/security",
+        'dst': INTERNXT_DRIVE,
+        'options': (
+            "-rpAXogEt",
+            "--delete",
+            "--exclude=security/archives", )
+    },
+    {
+        'src': USER_HOME + "/dev/cloudsync",
+        'dst': SPIDEROAK_HIVE + "/gitbackup",
+        'options': (
+            "-rpAXogEtc",
+            "--delete",
+            "--exclude=.git",
+            "--exclude=.idea",
+            "--exclude=venv",
+            "--exclude=__pycache__",
+            "--exclude=*.swp"
+        )
+    },
+    {
+        'src': USER_HOME + "/dev/joplinRevisionsCleanUp",
+        'dst': SPIDEROAK_HIVE + "/gitbackup",
+        'options': (
+            "-rpAXogEtc",
+            "--delete",
+            "--exclude=.git",
+            "--exclude=.idea",
+            "--exclude=venv",
+            "--exclude=*.swp"
+        )
+    },
+    {
+        'src': SPIDEROAK_HIVE + "/gitbackup",
+        'dst': INTERNXT_DRIVE,
+        'options': (
+            "-rpAXogEt",
+            "--delete"
+        )
+    }
+)
